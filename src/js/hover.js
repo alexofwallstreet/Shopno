df97f69e0ff12791658fa3cc77515e77473c706b
Alex <leshka-povareshka@yandex.ru.com>
2021-09-27 17:25:23 +0300

    fixed image on promo section

--- src/js/hover.js
+++ src/js/hover.js
@@ -3,7 +3,7 @@ const team = document.querySelectorAll(".team__list-item");
 team.forEach(team_member => {
     const hover = team_member.querySelector(".team__hover-window");
 
-    team_member.addEventListener("mouseover", () => {
+    team_member.addEventListener("mouseenter", () => {
         hover.classList.add("team__hover-window-show");
     });
 
@@ -11,7 +11,7 @@ team.forEach(team_member => {
 
     });
 
-    team_member.addEventListener("mouseout", () => {
+    team_member.addEventListener("mouseleave", () => {
         hover.classList.remove("team__hover-window-show");
     });
 })
